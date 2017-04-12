5d681e57caa37667ac64fb69eee67027b94bb0ef
Al-Zahir <alzahir97@gmail.com>
2017-04-12 18:54:11 -0400

    Al tutorial 6

--- Al/OpenGL/src/engineTester/MainGameLoop.java
+++ Al/OpenGL/src/engineTester/MainGameLoop.java
@@ -8,9 +8,11 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
+import models.RawModel;
+import models.TexturedModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 /**
  *
@@ -42,13 +44,24 @@ public class MainGameLoop {
             
         };
         
-        RawModel model = loader.loadToVAO(vertices, indices);
+        float[] textureCoords = {
+        
+            0, 0,  //V0
+            0, 1,  //V1
+            1, 1,  //V2
+            1, 0   //V3
+            
+        };
+        
+        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+        ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+        TexturedModel texturedModel = new TexturedModel(model, texture);
         
         while(!Display.isCloseRequested()){
             
             renderer.prepare();
             shader.start();
-            renderer.render(model);
+            renderer.render(texturedModel);
             shader.stop();
         
             //game logic
