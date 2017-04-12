8372f428f2977334ee77a420be70c7b6cd363c23
Al-Zahir <alzahir97@gmail.com>
2017-04-12 01:44:55 -0400

    Al tutorial 3

--- Al/OpenGL/src/engineTester/MainGameLoop.java
+++ Al/OpenGL/src/engineTester/MainGameLoop.java
@@ -7,6 +7,9 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 /**
  *
@@ -18,7 +21,31 @@ public class MainGameLoop {
         
         DisplayManager.createDisplay();
         
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+        
+        float[] vertices = {
+        
+            -0.5f, 0.5f, 0f,    //V0
+            -0.5f, -0.5f, 0f,   //V1
+            0.5f, -0.5f, 0f,    //V2
+            0.5f, 0.5f, 0f      //V3
+            
+        };
+        
+        int[] indices = {
+        
+            0, 1, 3,    //Top left triangle (V0, V1, V3)
+            3, 1, 2     //Bottom right triangle (V3, V1, V2)
+            
+        };
+        
+        RawModel model = loader.loadToVAO(vertices, indices);
+        
         while(!Display.isCloseRequested()){
+            
+            renderer.prepare();
+            renderer.render(model);
         
             //game logic
             //render
@@ -26,6 +53,7 @@ public class MainGameLoop {
             
         }
         
+        loader.cleanUp();
         DisplayManager.closeDisplay();
         
     }
