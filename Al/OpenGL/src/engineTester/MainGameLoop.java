9e34b4856086f5ab1820e74fcb75fd7307b075f8
Al-Zahir <alzahir97@gmail.com>
2017-04-13 00:52:12 -0400

    Tutorial 8

--- Al/OpenGL/src/engineTester/MainGameLoop.java
+++ Al/OpenGL/src/engineTester/MainGameLoop.java
@@ -5,11 +5,14 @@
  */
 package engineTester;
 
+import entities.Camera;
+import entities.Entity;
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import models.RawModel;
 import models.TexturedModel;
+import org.lwjgl.util.vector.Vector3f;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -19,61 +22,122 @@ import textures.ModelTexture;
  * @author Al-Zahir
  */
 public class MainGameLoop {
-    
+
     public static void main(String[] args) {
-        
+
         DisplayManager.createDisplay();
-        
+
         Loader loader = new Loader();
-        Renderer renderer = new Renderer();
         StaticShader shader = new StaticShader();
-        
+        Renderer renderer = new Renderer(shader);
+
         float[] vertices = {
-        
-            -0.5f, 0.5f, 0f,    //V0
-            -0.5f, -0.5f, 0f,   //V1
-            0.5f, -0.5f, 0f,    //V2
-            0.5f, 0.5f, 0f      //V3
-            
+            -0.5f, 0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, 0.5f, -0.5f,
+            -0.5f, 0.5f, 0.5f,
+            -0.5f, -0.5f, 0.5f,
+            0.5f, -0.5f, 0.5f,
+            0.5f, 0.5f, 0.5f,
+            0.5f, 0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, 0.5f,
+            0.5f, 0.5f, 0.5f,
+            -0.5f, 0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f, 0.5f,
+            -0.5f, 0.5f, 0.5f,
+            -0.5f, 0.5f, 0.5f,
+            -0.5f, 0.5f, -0.5f,
+            0.5f, 0.5f, -0.5f,
+            0.5f, 0.5f, 0.5f,
+            -0.5f, -0.5f, 0.5f,
+            -0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, 0.5f
+
         };
-        
-        int[] indices = {
-        
-            0, 1, 3,    //Top left triangle (V0, V1, V3)
-            3, 1, 2     //Bottom right triangle (V3, V1, V2)
-            
-        };
-        
+
         float[] textureCoords = {
-        
-            0, 0,  //V0
-            0, 1,  //V1
-            1, 1,  //V2
-            1, 0   //V3
-            
+            0, 0,
+            0, 1,
+            1, 1,
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
+            1, 0,
+            0, 0,
+            0, 1,
+            1, 1,
+            1, 0
+
         };
-        
+
+        int[] indices = {
+            0, 1, 3,
+            3, 1, 2,
+            4, 5, 7,
+            7, 5, 6,
+            8, 9, 11,
+            11, 9, 10,
+            12, 13, 15,
+            15, 13, 14,
+            16, 17, 19,
+            19, 17, 18,
+            20, 21, 23,
+            23, 21, 22
+
+        };
+
         RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+
         ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
-        TexturedModel texturedModel = new TexturedModel(model, texture);
-        
-        while(!Display.isCloseRequested()){
+
+        TexturedModel staticModel = new TexturedModel(model, texture);
+
+        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+
+        Camera camera = new Camera();
+
+        while (!Display.isCloseRequested()) {
             
+            entity.increaseRotation(1, 1, 0);
+
+            camera.move();
+
             renderer.prepare();
+
             shader.start();
-            renderer.render(texturedModel);
+            shader.loadViewMatrix(camera);
+
+            renderer.render(entity, shader);
+
             shader.stop();
-        
+
             //game logic
             //render
             DisplayManager.updateDisplay();
-            
+
         }
-        
+
         shader.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplay();
-        
+
     }
-    
+
 }
