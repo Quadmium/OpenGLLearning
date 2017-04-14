ba8be78490c80ceb273282db470adf2a8b5536c5
quadmium <quadmium@gmail.com>
2017-04-14 02:15:56 -0500

    Tutorial 11

--- Idan/OpenGL/src/engineTester/MainGameLoop.java
+++ Idan/OpenGL/src/engineTester/MainGameLoop.java
@@ -2,6 +2,7 @@ package engineTester;
 
 import entities.Camera;
 import entities.Entity;
+import entities.Light;
 import models.RawModel;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
@@ -23,11 +24,12 @@ public class MainGameLoop
         StaticShader shader = new StaticShader();
         Renderer renderer = new Renderer(shader);
         
-        RawModel model = OBJLoader.loadObjModel("stall", loader);
-        ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
+        RawModel model = OBJLoader.loadObjModel("dragon", loader);
+        ModelTexture texture = new ModelTexture(loader.loadTexture("white"));
         TexturedModel staticModel = new TexturedModel(model, texture);
         
-        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -20), 0, 0, 0, 1);
+        Entity entity = new Entity(staticModel, new Vector3f(0, -5, -25), 0, 0, 0, 1);
+        Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1, 1, 1));
         
         Camera camera = new Camera();
         
@@ -37,6 +39,7 @@ public class MainGameLoop
             camera.move();
             renderer.prepare();
             shader.start();
+            shader.loadLight(light);
             shader.loadViewMatrix(camera);
             renderer.render(entity, shader);
             shader.stop();
--- Idan/OpenGL/src/shaders/StaticShader.java
+++ Idan/OpenGL/src/shaders/StaticShader.java
@@ -1,6 +1,7 @@
 package shaders;
 
 import entities.Camera;
+import entities.Light;
 import org.lwjgl.util.vector.Matrix4f;
 import toolbox.Maths;
 
@@ -16,6 +17,8 @@ public class StaticShader extends ShaderProgram {
     private int location_transformationMatrix;
     private int location_projectionMatrix;
     private int location_viewMatrix;
+    private int location_lightPosition;
+    private int location_lightColor;
     
     public StaticShader() 
     {
@@ -27,6 +30,7 @@ public class StaticShader extends ShaderProgram {
     {
         super.bindAttribute(0, "position");
         super.bindAttribute(1, "textureCoords");
+        super.bindAttribute(2, "normal");
     }
 
     @Override
@@ -35,6 +39,8 @@ public class StaticShader extends ShaderProgram {
         location_transformationMatrix = super.getUniformLocation("transformationMatrix");
         location_projectionMatrix = super.getUniformLocation("projectionMatrix");
         location_viewMatrix = super.getUniformLocation("viewMatrix");
+        location_lightPosition = super.getUniformLocation("lightPosition");
+        location_lightColor = super.getUniformLocation("lightColor");
     }
     
     public void loadTransformationMatrix(Matrix4f matrix)
@@ -51,6 +57,12 @@ public class StaticShader extends ShaderProgram {
     {
         Matrix4f viewMatrix = Maths.createViewMatrix(camera);
         super.loadMatrix(location_viewMatrix, viewMatrix);
+    }
+    
+    public void loadLight(Light light)
+    {
+        super.loadVector(location_lightPosition, light.getPosition());
+        super.loadVector(location_lightColor, light.getColor());
     }
    
 }
